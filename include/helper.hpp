be6afd23ddab3f2dd08736ca85fcf1f902aaa79c
Derek G <45305531+dGrowl@users.noreply.github.com>
2020-03-01 21:48:53 -0800

    Added barebones Stopwatch class

--- include/helper.hpp
+++ include/helper.hpp
@@ -1,7 +1,29 @@
 #ifndef _LEET_HELPER_HPP_
 #define _LEET_HELPER_HPP_
 
+#include <chrono>
 #include <iostream>
+
+class Stopwatch {
+private:
+	std::chrono::time_point<std::chrono::steady_clock> begin;
+	std::chrono::time_point<std::chrono::steady_clock> end;
+public:
+	void start() {
+		begin = std::chrono::steady_clock::now();
+	}
+	void stop() {
+		end = std::chrono::steady_clock::now();
+	}
+	void reset() {
+		begin = std::chrono::time_point<std::chrono::steady_clock>();
+		end = std::chrono::time_point<std::chrono::steady_clock>();
+	}
+	auto report() {
+		std::chrono::duration<double> duration = end - begin;
+		return duration.count();
+	}
+};
 
 template <typename T> 
 std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) { 
